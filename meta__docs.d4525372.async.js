195870b17349e223452bbf56b19d8bb10b138680
tb-miao <2307057760@qq.com>
2025-03-22 11:38:38 +0800

    原始代码

---
+++ meta__docs.d4525372.async.js
@@ -0,0 +1,26 @@
+"use strict";(self.webpackChunkdocs_dumi=self.webpackChunkdocs_dumi||[]).push([[904],{97064:function(t,a,d){d.r(a),d.d(a,{demos:function(){return u}});var o=d(75271),n=d(88537),u={}},85752:function(t,a,d){d.r(a),d.d(a,{demos:function(){return u}});var o=d(75271),n=d(28568),u={}},60401:function(t,a,d){d.r(a),d.d(a,{demos:function(){return u}});var o=d(75271),n=d(49396),u={}},89168:function(t,a,d){d.r(a),d.d(a,{demos:function(){return u}});var o=d(75271),n=d(68526),u={}},34156:function(t,a,d){d.r(a),d.d(a,{demos:function(){return u}});var o=d(75271),n=d(9329),u={}},25571:function(t,a,d){d.r(a),d.d(a,{texts:function(){return n}});var o=d(88537);const n=[{value:"hello",paraId:0,tocIndex:0},{value:"Markdown \u8BED\u6CD5",paraId:1,tocIndex:2},{value:"\u914D\u7F6E\u9879",paraId:1,tocIndex:2},{value:"\u63D2\u4EF6",paraId:1,tocIndex:2}]},89667:function(t,a,d){d.r(a),d.d(a,{texts:function(){return n}});var o=d(28568);const n=[{value:"\u5B89\u88C5 ",paraId:0,tocIndex:0},{value:"Node.js",paraId:0,tocIndex:0},{value:`# \u5148\u627E\u4E2A\u5730\u65B9\u5EFA\u4E2A\u7A7A\u76EE\u5F55\u3002\r
+mkdir myapp && cd myapp\r
+\r
+# \u901A\u8FC7\u5B98\u65B9\u5DE5\u5177\u521B\u5EFA\u9879\u76EE\uFF0C\u9009\u62E9\u4F60\u9700\u8981\u7684\u6A21\u677F\r
+npx create-dumi\r
+\r
+# \u9009\u62E9\u4E00\u4E2A\u6A21\u677F\r
+? Pick template type \u203A - Use arrow-keys. Return to submit.\r
+\u276F   Static Site # \u7528\u4E8E\u6784\u5EFA\u7F51\u7AD9\r
+    React Library # \u7528\u4E8E\u6784\u5EFA\u7EC4\u4EF6\u5E93\uFF0C\u6709\u7EC4\u4EF6\u4F8B\u5B50\r
+    Theme Package # \u4E3B\u9898\u5305\u5F00\u53D1\u811A\u624B\u67B6\uFF0C\u7528\u4E8E\u5F00\u53D1\u4E3B\u9898\u5305\r
+\r
+# \u5B89\u88C5\u4F9D\u8D56\u540E\u542F\u52A8\u9879\u76EE\r
+npm start
+`,paraId:1,tocIndex:1},{value:"\u5B89\u88C5\u4E3B\u9898\uFF0C\u63A8\u8350\u4F7F\u7528dumi-theme-antd-style\u4E3B\u9898\u3002",paraId:2,tocIndex:2},{value:`pnpm i dumi-theme-antd-style -D
+`,paraId:3,tocIndex:2},{value:"\u5B89\u88C5dumi-theme-antd-style\u4E3B\u9898\u65F6\uFF0C\u7528\u65F6\u957F\u8BF7\u8010\u5FC3\u7B49\u5F85\u3002",paraId:4},{value:"\u4E3B\u9898\u6587\u6863: ",paraId:5},{value:"https://dumi-theme-antd-style.arvinx.com/",paraId:5},{value:"\u4E3B\u9898Github\u5730\u5740: ",paraId:5},{value:"https://github.com/arvinxx/dumi-theme-antd-style",paraId:5},{value:"dumi\u6587\u6863\u5730\u5740\uFF1A",paraId:5},{value:"dumi",paraId:5},{value:`dumi dev\r
+# \u542F\u52A8\u672C\u5730\u5F00\u53D1\u670D\u52A1\u5668\uFF0C\u8FDB\u884C\u9879\u76EE\u7684\u5F00\u53D1\u4E0E\u8C03\u8BD5\u3002
+`,paraId:6},{value:`dumi build\r
+# \u6784\u5EFA dumi \u6587\u6863\u4EA7\u7269\uFF0C\u9002\u7528\u4E8E\u751F\u4EA7\u73AF\u5883\u7684\u90E8\u7F72\u3002
+`,paraId:7},{value:`dumi preview\r
+# \u5728\u672C\u5730\u542F\u52A8\u4E00\u4E2A\u9759\u6001 Web \u670D\u52A1\u5668\uFF0C\u5C06 docs-dist \u6587\u4EF6\u5939\u8FD0\u884C\u5728 http://127.0.0.1:4172, \u7528\u4E8E\u9884\u89C8\u6784\u5EFA\u540E\u4EA7\u7269\u3002\r
+# \u4F60\u53EF\u4EE5\u901A\u8FC7 --port \u53C2\u6570\u6765\u914D\u7F6E\u670D\u52A1\u7684\u8FD0\u884C\u7AEF\u53E3\u3002
+`,paraId:8},{value:`dumi preview --port [\u7AEF\u53E3\u53F7]
+`,paraId:9},{value:`dumi version\r
+# \u67E5\u770B dumi \u7248\u672C\uFF0C\u7B49\u540C\u4E8E dumi -v\u3002
+`,paraId:10}]},53066:function(t,a,d){d.r(a),d.d(a,{texts:function(){return n}});var o=d(49396);const n=[{value:"\u4E0B\u8F7D\u5730\u5740\uFF1A",paraId:0,tocIndex:0},{value:"Ubuntu",paraId:0,tocIndex:0},{value:"\u9009\u62E9\u5408\u9002\u7684\u7248\u672C\u4E0B\u8F7D\uFF0C\u63A8\u835064\u4F4D\u7248\u672C\u3002",paraId:0,tocIndex:0},{value:"\u6253\u5F00\u4E0B\u8F7D\u597D\u7684iso\u955C\u50CF\uFF0C\u9009\u62E9\u5B89\u88C5Ubuntu\u3002",paraId:1,tocIndex:1},{value:"\u9009\u62E9\u8BED\u8A00\uFF0C\u4E2D\u6587\u7B80\u4F53\u3002",paraId:1,tocIndex:1},{value:"\u6309\u4E0B",paraId:2,tocIndex:3},{value:"F2",paraId:2,tocIndex:3},{value:"\u952E\uFF0C\u8FDB\u5165BIOS\u8BBE\u7F6E\u3002",paraId:2,tocIndex:3},{value:"\u9009\u62E9",paraId:2,tocIndex:3},{value:"Boot Options",paraId:2,tocIndex:3},{value:"\uFF0C\u9009\u62E9",paraId:2,tocIndex:3},{value:"UEFI Boot",paraId:2,tocIndex:3},{value:"\u3002",paraId:2,tocIndex:3},{value:"\u9009\u62E9",paraId:2,tocIndex:3},{value:"Save & Exit",paraId:2,tocIndex:3},{value:"\u3002",paraId:2,tocIndex:3},{value:"\u6309\u4E0B",paraId:2,tocIndex:3},{value:"F10",paraId:2,tocIndex:3},{value:"\u952E\uFF0C\u542F\u52A8Ubuntu\u3002",paraId:2,tocIndex:3},{value:"\u8F93\u5165\u7528\u6237\u540D\u548C\u5BC6\u7801\uFF0C\u8FDB\u5165\u684C\u9762\u3002",paraId:2,tocIndex:3}]},29168:function(t,a,d){d.r(a),d.d(a,{texts:function(){return n}});var o=d(68526);const n=[]},57866:function(t,a,d){d.r(a),d.d(a,{texts:function(){return n}});var o=d(9329);const n=[]}}]);
